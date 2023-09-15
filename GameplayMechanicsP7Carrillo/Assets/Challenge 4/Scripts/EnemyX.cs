4cc69d3d01bfe6c1b1368327a9fd48aa69464590
MackenzyCarrillo <709666@student.dusd.net>
2023-09-15 13:50:15 -0700

    Finished and recorded assignment
    finished and recorded assignment

--- GameplayMechanicsP7Carrillo/Assets/Challenge 4/Scripts/EnemyX.cs
+++ GameplayMechanicsP7Carrillo/Assets/Challenge 4/Scripts/EnemyX.cs
@@ -13,7 +13,7 @@ public class EnemyX : MonoBehaviour
     {
         enemyRb = GetComponent<Rigidbody>();
         playerGoal = GameObject.Find("Player Goal");
-        speed = Random.Range(20.0f, 500.0f);
+        speed = Random.Range(100.0f, 500.0f);
     }
 
     // Update is called once per frame
